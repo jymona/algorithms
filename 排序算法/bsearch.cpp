affc77c9da2a7a11391a3b78bc0f0a4968ee65ee
mona <66866763+jymona@users.noreply.github.com>
2023-11-20 23:33:00 -0500

    Update bsearch.cpp

--- 排序算法/bsearch.cpp
+++ 排序算法/bsearch.cpp
@@ -1,1 +1,22 @@
-int 
+// 区间[l,r]被划分为[l, mid]和[mid+1, r]时使用。
+int bsearch_1(int l, int r)
+{
+  while (l<r){
+    int mid = l+r >> 1;
+    if (check(mid)) r=mid;
+    else l=mid+1;
+  }
+  return l;
+}
+
+// 区间[l,r]被划分为[l, mid-1]和[mid, r]时使用。
+int bsearch_2(int l, int r)
+{
+  while (l<r){
+    int mid = l+r+1 >> 1;
+    if (check(mid)) l=mid;
+    else r=mid-1;
+  }
+  return l;
+}
+
